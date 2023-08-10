58bc98c1f5f8682a876da82f631589e40ce15e57
andaguiar <130407792+andaguiar@users.noreply.github.com>
2023-08-10 10:35:24 -0300

    aula 1

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../telas/telas_categoria.dart';
+
+void main() {
+  runApp(AppCardapio());
+}
+
+class AppCardapio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Cardápio",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TelasCategoria(),
+    );
+  }
+}
